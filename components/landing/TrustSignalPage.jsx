ed9ff49acb213123c6a3384001551fe279d90a0b
chrismaz11 <chrismaz11@me.com>
2026-05-29 14:23:10 +0000

    fix: logo size and pilot form design system

--- components/landing/TrustSignalPage.jsx
+++ components/landing/TrustSignalPage.jsx
@@ -4,7 +4,8 @@ import TrustSignalNav from "./TrustSignalNav";
 import TrustSignalFooter from "./TrustSignalFooter";
 import TrustSignalHero from "./TrustSignalHero";
 import TrustSignalAudience from "./TrustSignalAudience";
-import { PilotRequestSection } from "./pilot-request-section";
+import TrustSignalPilot from "./TrustSignalPilot";
+import TrustSignalPilotForm from "./TrustSignalPilotForm";
 import TrustSignalDevelopers from "./TrustSignalDevelopers";
 
 /**
@@ -23,7 +24,8 @@ export default function TrustSignalPage() {
       <TrustSignalNav />
       <TrustSignalHero />
       <TrustSignalAudience />
-      <PilotRequestSection />
+      <TrustSignalPilot />
+      <TrustSignalPilotForm />
       <TrustSignalDevelopers />
       <TrustSignalFooter />
     </div>
